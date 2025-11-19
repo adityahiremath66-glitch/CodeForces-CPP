25a822050329824e472661e3d44f7ed80fe620b6
adityahiremath66-glitch <adityahiremath66@gmail.com>
2025-11-19 15:14:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 30.7 MB (93.25%)

---
+++ solutions/3998-minimum-operations-to-equalize-array/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+    int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(nums[i] != nums[0]){
+                return 1;
+            }
+        }
+
+    return 0;
+    }
+};
