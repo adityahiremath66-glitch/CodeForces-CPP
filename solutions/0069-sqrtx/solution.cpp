f8b6335d81c02d75b0b85bce7e46edac7a63fc83
adityahiremath66-glitch <adityahiremath66@gmail.com>
2025-12-30 14:23:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.5 MB (85.45%)

--- solutions/0069-sqrtx/solution.cpp
+++ solutions/0069-sqrtx/solution.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        long long st = 0;
+    long long end = x,ans=0;
+        while(st<=end){
+        long long mid = (st+end)/2;
+        long long num_sq = mid*mid;
+            if(num_sq == x){
+                return mid;
+            }
+             if(num_sq < x){
+                st = mid+1;
+                ans=mid;
+            }else{
+                end = mid-1;
+            }
+        }
+        
+        return ans;
     }
 };
