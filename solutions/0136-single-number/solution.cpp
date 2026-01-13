b1e7261345d3a44a07eca3ca2a948c29f524f549
adityahiremath66-glitch <adityahiremath66@gmail.com>
2026-01-13 12:31:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.5 MB (98.51%)

--- solutions/0136-single-number/solution.cpp
+++ solutions/0136-single-number/solution.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-    int ans=0;
-	   for(auto x:nums){
-          ans = ans ^ x;
-       }
-	   return ans;
+    int n = nums.size();
+    sort(nums.begin(),nums.end());
+        for(int i=0; i<n-1; i+=2){
+            if(nums[i] != nums[i+1]){
+                return nums[i];
+            }
+        }
+        return nums[n-1];
     }
 };
