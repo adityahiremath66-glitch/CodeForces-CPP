8e52cff4271c7367d4feb96dc6e5a0b692168568
adityahiremath66-glitch <adityahiremath66@gmail.com>
2026-03-10 07:32:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.9 MB (53.69%)

---
+++ solutions/1078-remove-outermost-parentheses/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+    int n = s.length();
+    int count = 0;
+    string ans;
+        for(int i=0; i<n; i++){
+            if(s[i] == '('){
+                if(count){
+                    ans.push_back(s[i]);
+                }
+                count++;
+            }
+            if(s[i] == ')'){
+                count--;
+                if(count){
+                    ans.push_back(s[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
