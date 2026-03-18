af16612949093e82a304dd8ffb0944efff71dbd2
adityahiremath66-glitch <adityahiremath66@gmail.com>
2026-03-18 15:06:16 +0000

    Sync LeetCode submission Runtime - 55 ms (6.85%), Memory - 108.4 MB (13.15%)

---
+++ solutions/2362-minimum-rounds-to-complete-all-tasks/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& t) {
+    int n = t.size();
+    map<int,int> mp;
+        for(int i=0; i<n; i++){
+            mp[t[i]]++;
+        }
+    int ans = 0;
+        for(auto x : mp){
+            if(x.second == 1){
+                return -1;
+            }
+            else if(x.second % 3 == 0){
+                ans += x.second/3;
+            }
+            else{
+                ans += x.second/3 + 1;
+            }
+        }
+    return ans;
+    }
+};
