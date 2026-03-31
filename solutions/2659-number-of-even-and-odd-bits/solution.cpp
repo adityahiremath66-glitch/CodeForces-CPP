452ded14bc800dd60fe46bca2480d05050921f4c
adityahiremath66-glitch <adityahiremath66@gmail.com>
2026-03-31 09:19:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.7 MB (7.84%)

--- solutions/2659-number-of-even-and-odd-bits/solution.cpp
+++ solutions/2659-number-of-even-and-odd-bits/solution.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     vector<int> evenOddBit(int n) {
-    int a = n;
     vector<int> bin;
-    int even = 0;
-    int odd = 0;
-        while(a > 0){
-            bin.push_back(a % 2);
-            a /= 2;
+    int x = n;
+    int odd = 0,even = 0;
+        while(x){
+            int rem = x % 2;
+            bin.push_back(rem);
+            x /= 2;
         }
         for(int i=0; i<bin.size(); i++){
             if(bin[i] == 1 && i % 2 == 0){
                 even++;
-            }else if(bin[i] == 1 && i % 2 == 1){
-                odd++;
             }
+            if(bin[i] == 1 && i % 2 == 1){
+                odd++;
+            }   
         }
-    vector<int> ans = {even,odd};
-        return ans;
+        return {even,odd};
     }
 };
