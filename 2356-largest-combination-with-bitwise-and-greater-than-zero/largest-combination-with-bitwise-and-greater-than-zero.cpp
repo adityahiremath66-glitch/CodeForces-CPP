ec1fd93b0c5f2ec7ba8326173bcb252b7bc9d08f
adityahiremath66-glitch <adityahiremath66@gmail.com>
2026-07-15 20:37:22 +0530

    Time: 22 ms (84.13%) | Memory: 61.2 MB (80.69%) - LeetSync

---
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int largestCombination(vector<int>& c) {
+    int n = c.size();
+    int ans = 0;
+        for(int i=0; i<32; i++){
+            int cnt = 0;
+            for(auto x : c){
+                if(x & (1<<i)){
+                    cnt++;
+                }
+            }
+            ans = max(ans,cnt);
+        }
+    return ans;
+    }
+};
