8e95bcab899ff704503d5bbd1587fc999ef487aa
adityahiremath66-glitch <adityahiremath66@gmail.com>
2026-07-30 23:13:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (8.44%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     void merge(vector<int>& n1, int m, vector<int>& n2, int n) {
-    int j = 0;
-        for(int i=m; i<m+n; i++){
-            n1[i] = n2[j];
-            j++;
+    int i = m-1,j = n-1,idx=m+n-1;
+        while(i>=0 && j>=0){
+            if(n1[i] > n2[j]){
+                n1[idx] = n1[i];
+                idx--;
+                i--;
+            }else{
+                n1[idx] = n2[j];
+                idx--;
+                j--;
+            }
         }
-        sort(n1.begin(),n1.end());
+        while(j >= 0){
+            n1[idx] = n2[j];
+            idx--;
+            j--;
+        }
     }
 };
