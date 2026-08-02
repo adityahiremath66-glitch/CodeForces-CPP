2452263a68b8624dc6ba4bdbf33b2f1ce49d5b61
adityahiremath66-glitch <adityahiremath66@gmail.com>
2026-08-02 15:52:16 +0530

    Time: 96 ms (14.27%) | Memory: 18.6 MB (68.83%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& m, int tar) {
+    int n = m.size();
+    bool check = 0;
+        for(int i=0; i<n; i++){
+            if(check){
+                break;
+            }
+            check = binary_search(m[i].begin(),m[i].end(),tar);
+        }
+        return check;
+    }
+};
