50c1798585dc788b56097b5f2d03831a90a5eb30
adityahiremath66-glitch <adityahiremath66@gmail.com>
2026-09-22 20:00:46 +0530

    Time: 402 ms (16.02%) | Memory: 321.7 MB (29.98%) - LeetSync

---
+++ 4414-count-values-with-equally-spaced-occurrences-ii/count-values-with-equally-spaced-occurrences-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countSpecialIntegers(vector<int>& nums) {
+    int n = nums.size();
+    int ans = 0;
+    map<int,vector<int>> mp;
+        for(int i=0; i<n; i++){
+            mp[nums[i]].push_back(i);
+        }
+        for(auto x : mp){
+            if(x.second.size() >= 3){
+                bool check = true;
+                int a = x.second[1] - x.second[0];
+                for(int i=2; i<x.second.size(); i++){
+                    if(x.second[i] - x.second[i-1] != a){
+                        check = false;
+                        break;
+                    }
+                }
+                if(check) ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
